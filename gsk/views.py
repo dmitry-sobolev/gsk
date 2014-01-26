bccabedc7423b157a709f8cfbbd47399bc3528a7
Dmitry Sobolev <propp.ds@gmail.com>
2014-01-26 14:29:08 +0600

    В шаблоне корневой страницы отображается список гаражей

--- gsk/views.py
+++ gsk/views.py
@@ -1,6 +1,9 @@
 # Create your views here.
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+from gsk.models import Garage
 
 
-class RootView(TemplateView):
+class RootView(ListView):
     template_name = 'gsk/root.html'
+    model = Garage
+    context_object_name = 'garages'
